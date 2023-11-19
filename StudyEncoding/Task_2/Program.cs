9612652219115dad3669a8f9fbe309aee146dcb4
igoradm <igorpetrov42ru@gmail.com>
2023-11-19 17:23:58 +0300

    Task 2

---
+++ StudyEncoding/Task_2/Program.cs
@@ -0,0 +1,23 @@
+﻿ namespace Program
+{
+    using System;
+    using System.Text;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello World!";
+            Encoding unicode = Encoding.Unicode;
+            byte[] bytes = unicode.GetBytes(text);
+            foreach (byte c in bytes) {
+                Console.Write(c + " ");
+            }
+            Console.WriteLine();
+
+            Encoding ascii = Encoding.UTF7;
+            Console.WriteLine(ascii.GetString(bytes)); 
+
+            
+        }
+    }
+}
