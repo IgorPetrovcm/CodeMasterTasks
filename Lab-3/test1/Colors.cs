b029a69edbb08850a431f2ecd764dc7bd385fff9
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-21 20:39:51 +0300

    Add files via upload
    наработки для лабораторной 3, задание ColorReplacement

---
+++ Lab-3/test1/Colors.cs
@@ -0,0 +1,37 @@
+using System;
+namespace Program 
+{
+    class Color 
+    {
+        int redValue;
+        int greenValue;
+        int blueValue;
+        string name;
+
+        public int RedValue {get {return redValue;} private set {redValue = value;}}
+        public int GreenValue {get {return greenValue;} private set {greenValue = value;}}
+        public int BlueValue {get {return blueValue;} private set {blueValue = value;}}
+        public string Name {get {return name;} private set {name = value;}}
+
+        public Color(int redVal=0, int greenVal=0, int blueVal=0, string name=null) 
+        {
+            RedValue = redVal;
+            GreenValue = greenVal;
+            BlueValue = blueVal;
+            Name = name;
+        }
+
+        public void SetValues(string line) 
+        {
+            int indexEndName = line.IndexOf(' ');
+            Name = line.Substring(0, indexEndName);
+
+            int indexValues = line.IndexOf('#');
+            string lineValues = line.Substring(indexValues+1);
+
+            RedValue = Convert.ToInt32(lineValues[0].ToString() + lineValues[1].ToString(), 16);
+            GreenValue = Convert.ToInt32(lineValues[2].ToString() + lineValues[3].ToString(), 16);
+            BlueValue = Convert.ToInt32(lineValues[4].ToString() + lineValues[5].ToString(), 16);
+        }
+    }
+}
---
+++ Lab-3/test1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Program 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            string pathReadFile = @"C:\Users\Honor\Desktop\study C#\заметки\igor_petrov\Lab-3\ColorReplacement\Data\colors.txt";
+            using (var colorsRead = new StreamReader(pathReadFile, Encoding.UTF8)) 
+            {
+                string line;
+                while ((line = colorsRead.ReadLine()) != null) 
+                {
+                    int index2 = line.IndexOf(' ');
+                    string name = line.Substring(0, index2);
+                    System.Console.WriteLine(name);
+                    int index = line.IndexOf('#');
+                    string valColor = line.Substring(index+1);
+
+                    int valRed = Convert.ToInt32(valColor[0].ToString() + valColor[1].ToString(), 16);
+                    int valGreen = Convert.ToInt32(valColor[2].ToString() + valColor[3].ToString(),16);
+                    int valBlue = Convert.ToInt32(valColor[4].ToString() + valColor[5].ToString(),16);
+                    
+                    System.Console.WriteLine(valRed + " " + valGreen + " " + valBlue);
+                }
+            }
+        }
+    }
+}
