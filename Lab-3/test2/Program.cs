13ff57a11f378c84ad7fa5ccb48711d7d8b5a469
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-21 22:26:25 +0300

    Add files via upload

---
+++ Lab-3/test2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace Program 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            string path = @"C:\Users\Honor\Desktop\study C#\заметки\igor_petrov\Lab-3\ColorReplacement\Data\source.txt";
+
+            using (var reader = new StreamReader(path)) 
+            {
+                string line;                 
+                while ((line = reader.ReadLine()) != null) 
+                {
+                    Regex regex = new Regex(@"^rgb\(\d{1,3}\, \d{1,3}\, \d{1,3}\)");
+                    MatchCollection matches = regex.Matches(line);
+                    foreach (Match match in matches) {
+                        System.Console.WriteLine(match.Value);
+                    }
+                }
+            }
+        }
+    }
+}
