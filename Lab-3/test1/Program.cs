38a8d6c5d4e17b24707af862f31a2ee316656f8c
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-21 21:55:57 +0300

    Add files via upload

--- Lab-3/test1/Program.cs
+++ Lab-3/test1/Program.cs
@@ -1,29 +1,24 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 namespace Program 
 {
     class Program 
     {
         static void Main(string[] args) 
         {
-            string pathReadFile = @"C:\Users\Honor\Desktop\study C#\заметки\igor_petrov\Lab-3\ColorReplacement\Data\colors.txt";
+            string pathReadFile = @"C:\Users\Honor\Desktop\study C#\заметки\igor_petrov\Lab-3\ColorReplacement\Data\source.txt";
             using (var colorsRead = new StreamReader(pathReadFile, Encoding.UTF8)) 
             {
-                string line;
+                string line;                
                 while ((line = colorsRead.ReadLine()) != null) 
                 {
-                    int index2 = line.IndexOf(' ');
-                    string name = line.Substring(0, index2);
-                    System.Console.WriteLine(name);
-                    int index = line.IndexOf('#');
-                    string valColor = line.Substring(index+1);
-
-                    int valRed = Convert.ToInt32(valColor[0].ToString() + valColor[1].ToString(), 16);
-                    int valGreen = Convert.ToInt32(valColor[2].ToString() + valColor[3].ToString(),16);
-                    int valBlue = Convert.ToInt32(valColor[4].ToString() + valColor[5].ToString(),16);
-                    
-                    System.Console.WriteLine(valRed + " " + valGreen + " " + valBlue);
+                    Regex regex = new Regex(@"^rgb\(\d{1,3}\, \d{1,3}\, \d{1,3}\)");
+                    MatchCollection matchCollection = regex.Matches(line);
+                    foreach (Match match in matchCollection) {
+                        System.Console.WriteLine(match.Value);
+                    }
                 }
             }
         }
