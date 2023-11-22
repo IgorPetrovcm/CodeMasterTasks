db0a5273185e2423970ba3982836e3bd49210ab9
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-22 18:05:25 +0300

    Add files via upload

--- Lab-3/test4/Program.cs
+++ Lab-3/test4/Program.cs
@@ -15,11 +15,21 @@ namespace Program
                 {
                     string regex1 = @"rgb\(\d{1,3}\, \d{1,3}\, \d{1,3}\)";
                     string regex2 = @"\#\w{6}";
+                    string regex3 = @"\#\w{3}";
                     /*foreach (Match match in Regex.Matches(line,regex1)) {
                         System.Console.WriteLine(match.Value);
                     }*/
-                    foreach (Match match in Regex.Matches(line,regex2)) {
+                    /*foreach (Match match in Regex.Matches(line,regex2)) {
                         System.Console.WriteLine(Convert.ToInt32(match.Value.Substring(1).ToUpper(), 16));
+                    }*/
+                    foreach (Match match in Regex.Matches(line,regex3)) {
+                        string handlerMatch = match.Value.Substring(1);
+                        for (int i = 0; i < handlerMatch.Length; i++) 
+                        {
+                            handlerMatch = handlerMatch.Insert(i,handlerMatch[i].ToString());
+                            i++;
+                        }
+                        System.Console.WriteLine(handlerMatch);
                     }
                 }
                 
