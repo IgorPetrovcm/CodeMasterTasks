b38d551be4564b396c95368f593c373995f392b2
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-11-28 15:01:51 +0300

    Add files via upload

---
+++ Lab-3/WordCout/testReadAnsi/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+namespace Program;
+class Program 
+{
+    static void Main() 
+    {
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        string text = File.ReadAllText(@"C:\Users\Honor\Desktop\study C#\заметки\igor_petrov\Lab-3" + 
+        @"\WordCount\Data\Начало.txt",Encoding.GetEncoding(1251));
+        System.Console.WriteLine(text);
+    }
+}
