92992d5ce9c42b7824ffd3e36747fff0d3098c41
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-07 13:16:47 +0300

    Рабочий класс

---
+++ Lab4/NodeList3/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Program 
+{
+    class Program 
+    {
+        static void Main() 
+        {
+            NodeList<string> list = new NodeList<string>();
+            list.Add("vlad"); 
+            list.Add("vlad1");
+            list.Add("vlad2");
+            list.Remove("vlad1");
+            bool b = list.Contains("vlad1");
+        }
+    }
+}
