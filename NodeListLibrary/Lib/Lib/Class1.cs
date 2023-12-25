2586398e221c3e54358d5e5b5d2b54d70cd32b5a
IgorPetrovcm <144310791+IgorPetrovcm@users.noreply.github.com>
2023-12-25 23:09:07 +0300

    First library prototype

---
+++ NodeListLibrary/Lib/Lib/Class1.cs
@@ -0,0 +1,57 @@
+﻿namespace NodeListLib;
+
+public class Node<T>
+{
+    public T value;
+
+    public Node<T> next ;
+
+    public Node(T value, Node<T> next)
+    {
+        this.value = value;
+        this.next = next;
+    }
+    public Node(T value)
+    {
+        this.value = value;
+    }
+    public Node()
+    {
+
+    }
+}
+public class NodeList<T>
+{
+    private Node<T> head;
+
+    private Node<T> tail ;
+
+    private int count;
+    public int Count { get => count; }
+
+    public NodeList() {  }
+
+    public void Add(T value)
+    {
+        if (head == null) throw new NullReferenceException("Add value id empty");
+
+        Node<T> node = new Node<T>(value);
+
+        if (head == null)
+        {
+            head = node;
+            head.next = node;
+            tail = node;
+
+            count++;
+
+            return;
+        }
+
+        tail.next = node;
+        tail = node;
+        
+        count++;
+    }
+
+}
