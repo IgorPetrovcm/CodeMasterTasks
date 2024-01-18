bcb696cf9ffbe36ccce50f70c82c17cfe6cd184f
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-01-18 20:09:26 +0300

    update diagramm, update several files in Scene2d

---
+++ Lab4/Scene2d/Scene2d/Figures/CircleFigure.cs
@@ -0,0 +1,55 @@
+namespace Scene2d.Figures
+{
+    using System.Drawing;
+
+    public class CircleFigure : IFigure
+    {
+        private ScenePoint _center;
+        private double _radius;
+
+        public CircleFigure(ScenePoint center, double radius)
+        {
+            _center = center;
+            _radius = radius;
+        }
+
+        public SceneRectangle CalculateCircumscribingRectangle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Clone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw(ScenePoint origin, Graphics drawing)
+        {
+            /* Already implemented */
+            using (var pen = new Pen(Color.Green))
+            {
+                drawing.DrawEllipse(
+                    pen,
+                    (int)(_center.X - _radius - origin.X),
+                    (int)(_center.Y - _radius - origin.Y),
+                    (int)(_radius * 2),
+                    (int)(_radius * 2));
+            }
+        }
+
+        public void Move(ScenePoint vector)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reflect(ReflectOrientation orientation)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate(double angle)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Lab4/Scene2d/Scene2d/Figures/PolygonFigure.cs
@@ -0,0 +1,61 @@
+namespace Scene2d.Figures
+{
+    using System.Drawing;
+    using System.Linq;
+
+    public class PolygonFigure : IFigure
+    {
+        private ScenePoint[] _points;
+
+        public PolygonFigure(ScenePoint[] points)
+        {
+            _points = points;
+        }
+
+        public SceneRectangle CalculateCircumscribingRectangle()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public object Clone()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw(ScenePoint origin, Graphics drawing)
+        {
+            /* Already implemented */
+
+            using (var pen = new Pen(Color.DarkOrchid))
+            {
+                for (var i = 0; i < _points.Length; i++)
+                {
+                    ScenePoint firstPoint = _points[i];
+                    ScenePoint secondPoint = i >= _points.Length - 1 ? _points.First() : _points[i + 1];
+
+                    drawing.DrawLine(
+                        pen,
+                        (float)(firstPoint.X - origin.X),
+                        (float)(firstPoint.Y - origin.Y),
+                        (float)(secondPoint.X - origin.X),
+                        (float)(secondPoint.Y - origin.Y));
+                }
+            }
+        }
+
+        public void Move(ScenePoint vector)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reflect(ReflectOrientation orientation)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Rotate(double angle)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
