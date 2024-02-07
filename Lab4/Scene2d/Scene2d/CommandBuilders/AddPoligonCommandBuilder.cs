9413d4a0d82d21eb1c32c2774542c32df4d200a4
Honor <igorpetrov42ru@gmail.com>
2024-02-07 21:34:14 +0300

    Допилил AppendLine для многоугольника и протестил, все работает

--- Lab4/Scene2d/Scene2d/CommandBuilders/AddPoligonCommandBuilder.cs
+++ Lab4/Scene2d/Scene2d/CommandBuilders/AddPoligonCommandBuilder.cs
@@ -1,6 +1,6 @@
 namespace Scene2d.CommandBuilders 
 {
-
+    using System.Collections.Generic;
     using System.Text.RegularExpressions;
     using Scene2d.Commands;
     using Scene2d.Exceptions;
@@ -8,29 +8,44 @@ namespace Scene2d.CommandBuilders
 
     public class AddPoligonCommandBuilder : ICommandBuilder
     {
-        private static readonly Regex RecognizeRegex = new Regex(@"^(add polygon) ([a-zA-Z0-9\-_]+)|
-            (add point) \((-?\d+),\s?(-?\d+)\)|(end polygon)");
+        private static readonly Regex RecognizeRegex = new Regex(@"^(?:add polygon ([a-zA-Z0-9\-_]+)|add point \((-?\d+),\s?(-?\d+)\)|end polygon)");
 
         private bool _isStarted = false; 
+
+        private string _name;
+
+        private IFigure _polygon;
+
+        private List<ScenePoint> _points = new List<ScenePoint>();
 
         public bool IsCommandReady 
         {
             get 
             {
-                if (_isStarted)
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
+                return !_isStarted;
             }
         }
 
         public void AppendLine(string line)
         {
             Match match = RecognizeRegex.Match(line);
+
+            if (line.StartsWith("add polygon"))
+            {
+                _name = match.Groups[1].Value;
+
+                _isStarted = true;
+            }
+            else if (line.StartsWith("add point"))
+            {
+                _points.Add(new ScenePoint(double.Parse(match.Groups[2].Value), double.Parse(match.Groups[3].Value)));
+            }
+            else if (line.StartsWith("end polygon"))
+            {
+                _isStarted = false;
+
+                _polygon = new PolygonFigure(_points.ToArray());
+            }
         }
 
         public ICommand GetCommand()
