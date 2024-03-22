94342d7f2944611a5e5a4f1bcdf36e24d3d274ac
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-03-22 20:20:37 +0300

    Выполнил Lab-7 Gates

--- Lab-7/GatesController/GatesController.Core/GatesManager.cs
+++ Lab-7/GatesController/GatesController.Core/GatesManager.cs
@@ -2,6 +2,8 @@ namespace GatesController.Core
 {
     public class GatesManager
     {
+        object? lockObject = new object();
+        
         private readonly ILogger _logger;
 
         private GateStatus[] _gates = new GateStatus[5];
@@ -29,37 +31,101 @@ namespace GatesController.Core
         // Open Request logic
         public async Task RequestOpen(int gateId)
         {
-            _logger.LogInformation("Received open request for Gate {GateId}", gateId);
+            lock(lockObject)
+            {
+                if (gateId < 0 || gateId > 5)
+                {
+                    _logger.LogInformation("There is no gate with this id");
+                    return;
+                }
+
+                int gatesOpenCount = 0;
+                int gateOpen = 0;
+
+                for (int i = 0; i < _gates.Length; i++)
+                {
+                    if (_gates[i] == GateStatus.Open)
+                    {
+                        gateOpen = i + 1;
+                        gatesOpenCount++;
+                    }
+             }
+
+                if (gatesOpenCount == 2)
+                {
+                    _logger.LogInformation("You will not be able to open more gates");
+                    return;
+                }
+
+                if (gateId == gateOpen + 1 || gateId == gateOpen - 1)
+                {
+                    _logger.LogInformation("The neighboring gate cannot be opened");
+                }   
+            
+                _logger.LogInformation("Received open request for Gate {GateId}", gateId);
 
             // We need to set the Status to waiting in case the gate is blocked
             // _gates[gateId - 1] = GateStatus.Waiting;
-            _gates[gateId - 1] = GateStatus.Open;  // Make sure we set correct status for Dashboard to work
+                _gates[gateId - 1] = GateStatus.Open;  // Make sure we set correct status for Dashboard to work
 
-            _logger.LogInformation("Gate {GateId} is open", gateId);
+                _logger.LogInformation("Gate {GateId} is open", gateId);
+            }
         }
 
         // Close Request logic
         public async Task RequestClose(int gateId)
         {
-            _logger.LogInformation("Received close request for Gate {GateId}", gateId);
+            lock (lockObject)
+            {
+                if (gateId < 0 || gateId > 5)
+                {
+                    _logger.LogInformation("There is no gate with this id");
+                    return;
+                }
 
-            _logger.LogInformation("Gate {GateId} is closed", gateId);
+                _logger.LogInformation("Received close request for Gate {GateId}", gateId);
 
-            _gates[gateId - 1] = GateStatus.Closed; // Make sure we set correct status for Dashboard to work
+                _logger.LogInformation("Gate {GateId} is closed", gateId);
+
+                _gates[gateId - 1] = GateStatus.Closed; // Make sure we set correct status for Dashboard to work
+            }
         }
 
         // Register Enter Request
         public void RegisterVisitorEnter(int gateId)
         {
-            _visitorsCount++;
-            _visitorsByGates[gateId - 1]++;
+            lock (lockObject)
+            {
+                if (gateId < 0 || gateId > 5)
+                {
+                    _logger.LogInformation("There is no gate with this id");
+                    return;
+                }
+                _visitorsCount++;
+                _visitorsByGates[gateId - 1]++;
+            }
         }
 
         // Register Leave Request
         public void RegisterVisitorLeave(int gateId)
         {
-            _visitorsCount--;
-            _visitorsByGates[gateId - 1]--;
+            lock (lockObject)
+            {
+                if (gateId < 0 || gateId > 5)
+                {
+                    _logger.LogInformation("There is no gate with this id");
+                    return;
+                }
+
+                if (_visitorsByGates[gateId - 1] <= 0)
+                {
+                    _logger.LogInformation("The number of visitors is zero");
+                    return;
+                }
+
+                _visitorsCount--;
+                _visitorsByGates[gateId - 1]--;
+            }
         }
     }
 }
--- Lab-7/GatesController/GatesController.Core/Program.cs
+++ Lab-7/GatesController/GatesController.Core/Program.cs
@@ -11,27 +11,34 @@ app.MapGet("/", () => Results.Redirect("index.html"));
 
 app.MapGet(
     "/api/request/open/{gateId:int}",
-    (int gateId, [FromServices] GatesManager gatesManager) => gatesManager.RequestOpen(gateId));
+    (int gateId, [FromServices] GatesManager gatesManager) => {
+            gatesManager.RequestOpen(gateId);
+        });
 
 app.MapGet(
     "/api/request/close/{gateId:int}",
-    (int gateId, [FromServices] GatesManager gatesManager) => gatesManager.RequestClose(gateId));
+    (int gateId, [FromServices] GatesManager gatesManager) => {
+            gatesManager.RequestClose(gateId);
+    } );
 
 app.MapGet(
     "/api/status",
-    ([FromServices] GatesManager gatesManager) => new
-    {
-        Gates = gatesManager.GetOpenGates(),
-        VisitorsTotal = gatesManager.VisitorsCount,
-        VisitorsByGates = gatesManager.VisitorsByGates,
+    ([FromServices] GatesManager gatesManager) =>new {
+                Gates = gatesManager.GetOpenGates(),
+                VisitorsTotal = gatesManager.VisitorsCount,
+                VisitorsByGates = gatesManager.VisitorsByGates,
     });
 
 app.MapGet(
     "/api/enter/{gateId:int}",
-    (int gateId, [FromServices] GatesManager gatesManager) => gatesManager.RegisterVisitorEnter(gateId));
+    (int gateId, [FromServices] GatesManager gatesManager) => {
+            gatesManager.RegisterVisitorEnter(gateId);
+        });
 
 app.MapGet(
     "/api/leave/{gateId:int}",
-    (int gateId, [FromServices] GatesManager gatesManager) => gatesManager.RegisterVisitorLeave(gateId));
+    (int gateId, [FromServices] GatesManager gatesManager) => {
+            gatesManager.RegisterVisitorLeave(gateId);
+        });
 
 app.Run();
