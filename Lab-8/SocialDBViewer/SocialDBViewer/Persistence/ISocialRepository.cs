d1b398f6bb9c6041ec8ff4d3bedab9eeb19e42d6
IgorPetrovcm <igorpetrov42ru@gmail.com>
2024-03-28 18:09:36 +0300

    Дореализовал паттерн репозиторий

--- Lab-8/SocialDBViewer/SocialDBViewer/Persistence/ISocialRepository.cs
+++ Lab-8/SocialDBViewer/SocialDBViewer/Persistence/ISocialRepository.cs
@@ -5,4 +5,26 @@ using SocialDBViewer.Domain;
 public interface ISocialRepository
 {
     Task<User> AddUser(User user);
+    
+    Task<IEnumerable<User>> GetUsers();
+
+    Task DeleteUser(int idUser);
+
+    Task<Friend> AddFriend(Friend friend);
+
+    Task<IEnumerable<Friend>> GetFriends();
+
+    Task DeleteFriend(int friendId);
+
+    Task<Message> AddMessage(Message message);
+
+    Task<IEnumerable<Message>> GetMessages();
+
+    Task DeleteMessage(int messageId);
+
+    Task<Like> AddLike(Like like);
+
+    Task<IEnumerable<Like>> GetLikes();
+
+    Task DeleteLike(int likeId);
 }
